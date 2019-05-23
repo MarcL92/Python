94dbe458e39d14aacee67b47532ac7810eefe7f3
MarcL92 <92.marek.l@gmail.com>
2019-05-23 22:51:18 +0200

    Zmiana w pliku main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ test1 = 1
 test = 2
 
 print("Aśka gamoniu idź spać")
+
+
+#commitujemy -> git commit [plik] lub git commit -A
+#DODAJEMY WIADOMOSC W SENSIE OPIS ZMIAN  ESQ :wq
+#git push [nazwapliku] LUB git push --all
+#git status
